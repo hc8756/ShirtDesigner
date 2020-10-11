ef353dfe8ab620e9585b6334d70af9cce84d79d9
Hyunseo Cho <hc8756@rit.edu>
2020-10-11 08:41:27 -0400

    you can now import png images

---
+++ shirt_designer/Assets/Scripts/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.Networking;
+public class FileManager : MonoBehaviour
+{
+    string path;
+    public RawImage image;
+    public void OpenExplorer() {
+        path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+        GetImage();
+    }
+
+    void GetImage() {
+        if (path != null) {
+            StartCoroutine(UpdateImage());
+        }
+    }
+    IEnumerator UpdateImage() {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("file:///" + path);
+        yield return www.SendWebRequest();
+        image.texture = DownloadHandlerTexture.GetContent(www);
+        //this code makes images proportionate but often makes it too big
+        /*int w=DownloadHandlerTexture.GetContent(www).width;
+        int h = DownloadHandlerTexture.GetContent(www).height;
+        image.GetComponent<RectTransform>().sizeDelta = new Vector2(w, h);*/
+        
+    }
+}
