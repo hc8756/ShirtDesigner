9d54e9a2268ca088619bb713583e35e723f80b02
Hyunseo Cho <hc8756@rit.edu>
2020-11-19 11:30:04 -0500

    Basic drawing implemented

---
+++ shirt_designer/Assets/Scripts/ModelRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRotator : MonoBehaviour
+{
+    public KeyCode pressRight;
+    public KeyCode pressLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(pressLeft)) {
+            transform.Rotate(0.0f, 0.0f, 1.0f, Space.Self);
+        }
+
+        if (Input.GetKey(pressRight))
+        {
+            transform.Rotate(0.0f, 0.0f, -1.0f, Space.Self);
+        }
+
+    }
+}
